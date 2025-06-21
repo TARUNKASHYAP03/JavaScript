3dd46e26198794e11bfb76595110cb170ca9a170
Kapil Kashyap <kashyaptk7@gmail.com>
2025-06-21 10:27:19 +0530

    Learn Basic of Variable

---
+++ Basic/script.js
@@ -0,0 +1,20 @@
+console.log("Tarun Kumar")
+
+const userId=123
+
+let useEmail="tarun@gmail.com"
+
+console.log(userId,useEmail)
+
+userCity="Ghaziabad"
+
+let userAccount="123456789"
+let userPassword="123456"
+let userCountry;
+console.table([userId,useEmail,userCity,userAccount,userPassword,userCountry])
+
+/*
+Only use const and let 
+var is create issue bcause it's replace the value
+and create issue in block scope and functional scope
+*/
