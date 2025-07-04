8f4ce21002a9b3e77663bbbba9ce7e432cff734e
Kapil Kashyap <kashyaptk7@gmail.com>
2025-07-04 12:32:19 +0530

    Pattern Done

--- DSA/Pattern/Butterfly.js
+++ DSA/Pattern/Butterfly.js
@@ -1,18 +1,14 @@
-function upparBt(n) {
+function upperBt(n) {
   for (let i = 1; i <= (n - 1) / 2; i++) {
     let str = "";
-    let leftstar = i;
-    for (let k = 1; k <= leftstar; k++) {
+    for (let k = 1; k <= i; k++) {
       str += "*";
     }
     let sp = n - 2 * i;
-
     for (let j = 1; j <= sp; j++) {
       str += " ";
     }
-
-    let rightStar = i;
-    for (let l = 1; l <= rightStar; l++) {
+    for (let l = 1; l <= i; l++) {
       str += "*";
     }
     console.log(str);
@@ -28,27 +24,30 @@ function middBt(n) {
 }
 
 function lowerBt(n) {
-  for (let i = (n-2)/2; i <=1 ; i--) {
+  for (let i = (n - 1) / 2; i >= 1; i--) {
     let str = "";
-    let leftstar = i;
-    for (let k = 1; k <= leftstar; k++) {
+    for (let k = 1; k <= i; k++) {
       str += "*";
     }
     let sp = n - 2 * i;
-
     for (let j = 1; j <= sp; j++) {
       str += " ";
     }
-
-    let rightStar = i;
-    for (let l = 1; l <= rightStar; l++) {
+    for (let l = 1; l <= i; l++) {
       str += "*";
     }
     console.log(str);
   }
 }
 
-const a=5
-upparBt(a);
-middBt(a);
-lowerBt(a)
+function printP(a) {
+  if (a % 2 !== 0) {
+    upperBt(a);
+    middBt(a);
+    lowerBt(a);
+  } else {
+    console.log("Only print odd number");
+  }
+}
+
+printP(11);
